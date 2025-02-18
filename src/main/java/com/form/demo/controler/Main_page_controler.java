27f9700f7789a133cea7b8235a74cceaebce16c0
Deniz Mirza <denizimmirza@gmail.com>
2025-02-18 23:58:03 +0300

    first commit

---
+++ src/main/java/com/form/demo/controler/Main_page_controler.java
@@ -0,0 +1,36 @@
+package com.form.demo.controler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+public class Main_page_controler {
+    @RequestMapping("/")
+    public String requestMethodName() {
+        return "hello";}
+    @RequestMapping("/tr")
+    public String requestMethodName2() {
+        return "merhaba";
+    }
+    @RequestMapping("/fr")
+    public String requestMethodName3() {
+        return "front";
+    }
+    @RequestMapping("/byname")
+    public String requestMethodName4(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "hello " + name;
+    }
+    @RequestMapping("/square")
+    public int requestMethodName5(@RequestParam(value = "number", defaultValue = "0") int number) {
+        return number * number;
+    }
+    @RequestMapping("/plus")
+    public int requestMethodName6(@RequestParam(value = "number", defaultValue = "0") int number,@RequestParam(value = "number2", defaultValue = "1") int number2) {
+        return number + number2;
+    }
+    //requestmapping içine nasıl 2 parametre koyucağmı bulamadım
+}
+
